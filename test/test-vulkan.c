27d28d65a6154013fef921ad17cb95f61e0d7bf7
Xichen Zhou <xeechou@xzhou.net>
2021-02-04 22:30:52 +0800

    updating test

--- test/test-vulkan.c
+++ test/test-vulkan.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 	struct tw_vk_option opt = {
 		.requested_exts = TW_VK_WANT_VALIDATION_LAYER,
 		.instance_name = "test-vulkan",
+		.vendor_id = 0x8086,
+		.device_id = 0x3e9b,
 	};
 
 	tw_vk_init(&vk, &opt);
